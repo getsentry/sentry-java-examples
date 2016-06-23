f3c747ebb9f7887d6940d079ddd53b5ff974db1d
Brett Hoerner <brett@bretthoerner.com>
2016-06-23 10:11:31 -0500

    Bump examples to 7.3.0

--- raven-clojure-example/project.clj
+++ raven-clojure-example/project.clj
@@ -3,5 +3,5 @@
   :dependencies [
     [org.clojure/clojure "1.7.0"]
     [org.clojure/tools.logging "0.3.1"]
-    [com.getsentry.raven/raven-logback "7.2.1"]]
+    [com.getsentry.raven/raven-logback "7.3.0"]]
   :main raven-clojure-example.core)
