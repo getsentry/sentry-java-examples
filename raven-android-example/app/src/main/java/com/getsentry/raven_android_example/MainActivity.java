c83b712f9444986e1bc67b9f0cfa1b26becff1b3
Brett Hoerner <brett@bretthoerner.com>
2017-02-07 18:19:33 -0600

    Add new Android sample application. (#4)

---
+++ raven-android-example/app/src/main/java/com/getsentry/raven_android_example/MainActivity.java
@@ -0,0 +1,64 @@
+package com.getsentry.raven_android_example;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.getsentry.raven.android.Raven;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize the Raven client
+        Raven.init(this.getApplicationContext());
+
+        // Manually catch and send an exception
+        try {
+            int x = 1 / 0;
+        } catch (Exception e) {
+            Raven.capture(e);
+        }
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Clicking this will throw an unhandled exception that Raven will send to the Sentry server
+                throw new RuntimeException("Button press caused an exception!");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
